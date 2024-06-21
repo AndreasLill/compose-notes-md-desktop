0655efd2dfb34d61d4b9fac0e56f0698ed2af967
Andreas Lill <dev@andreaslill.com>
2024-06-21 13:39:19 +0200

    Changed gradle to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ compose.desktop {
         mainClass = "MainKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.AppImage)
             packageName = "ComposeNotesMD"
             packageVersion = "1.0.0"
         }
