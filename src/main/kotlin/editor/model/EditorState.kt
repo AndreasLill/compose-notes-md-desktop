fdc5ce22771aef9c2170254a102af06c1c579742
Andreas Lill <dev@andreaslill.com>
2024-07-11 23:44:56 +0200

    Refactored editor ui and state.

--- src/main/kotlin/editor/model/EditorState.kt
+++ src/main/kotlin/editor/model/EditorState.kt
@@ -3,10 +3,61 @@ package editor.model
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.withStyle
 
 class EditorState {
-    val colorText by mutableStateOf(Color(0xFFFAFAFA))
-    val colorHeader by mutableStateOf(Color(0xFF81D4FA))
-    val colorList by mutableStateOf(Color(0xFFCE93D8))
-    val colorBlockQuote by mutableStateOf(Color(0xFFA5D6A7))
+    private val colorText by mutableStateOf(Color(0xFFFAFAFA))
+    private val colorHeader by mutableStateOf(Color(0xFF81D4FA))
+    private val colorList by mutableStateOf(Color(0xFFCE93D8))
+    private val colorBlockQuote by mutableStateOf(Color(0xFFA5D6A7))
+
+    /**
+     * Translates markdown source text to annotated string format used by text field.
+     */
+    fun getMarkdownAnnotatedString(text: String): AnnotatedString {
+        val builder = AnnotatedString.Builder()
+        val lines = text.lines()
+
+        lines.forEachIndexed { index, line ->
+            when {
+                line.startsWith("#") -> {
+                    buildLine(builder, SpanStyle(colorHeader), line, !isLastLine(index, lines.size))
+                }
+                line.startsWith("*") -> {
+                    buildLine(builder, SpanStyle(colorList), line.substring(0, 1))
+                    buildLine(builder, SpanStyle(colorText), line.substring(1, line.length), !isLastLine(index, lines.size))
+                }
+                line.startsWith(">") -> {
+                    buildLine(builder, SpanStyle(colorBlockQuote), line.substring(0, 1))
+                    buildLine(builder, SpanStyle(colorText), line.substring(1, line.length), !isLastLine(index, lines.size))
+                }
+                else -> {
+                    buildLine(builder, SpanStyle(colorText), line, !isLastLine(index, lines.size))
+                }
+            }
+        }
+
+        return builder.toAnnotatedString()
+    }
+
+    /**
+     * Builds annotated string with provided style and handles line breaks.
+     */
+    private fun buildLine(builder: AnnotatedString.Builder, style: SpanStyle, text: String, breakLine: Boolean = false) {
+        builder.withStyle(style) {
+            append(text)
+
+            if (breakLine)
+                append("\n")
+        }
+    }
+
+    /**
+     * Checks if the current index is the last.
+     */
+    private fun isLastLine(index: Int, size: Int): Boolean {
+        return index == size - 1
+    }
 }
--- src/main/kotlin/editor/ui/Editor.kt
+++ src/main/kotlin/editor/ui/Editor.kt
@@ -12,13 +12,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.FileHandler
@@ -29,7 +26,7 @@ import application.model.Action
 
 @Composable
 fun Editor(appState: ApplicationState) {
-    val state = remember { EditorState() }
+    val editorState = remember { EditorState() }
     val scope = rememberCoroutineScope()
 
     LaunchedEffect(appState.file) {
@@ -65,7 +62,7 @@ fun Editor(appState: ApplicationState) {
                 cursorBrush = SolidColor(MaterialTheme.colors.primary),
                 visualTransformation = {
                     TransformedText(
-                        text = markdownAnnotatedString(state, appState.fileText.text),
+                        text = editorState.getMarkdownAnnotatedString(appState.fileText.text),
                         offsetMapping = OffsetMapping.Identity
                     )
                 }
@@ -109,52 +106,4 @@ fun Editor(appState: ApplicationState) {
             }
         }
     }
-}
-
-/**
- * Translates markdown source text to annotated string format used by text field.
- */
-fun markdownAnnotatedString(state: EditorState, text: String): AnnotatedString {
-    val builder = AnnotatedString.Builder()
-    val lines = text.lines()
-
-    lines.forEachIndexed { index, line ->
-        when {
-            line.startsWith("#") -> {
-                buildLine(builder, SpanStyle(state.colorHeader), line, !isLastLine(index, lines.size))
-            }
-            line.startsWith("*") -> {
-                buildLine(builder, SpanStyle(state.colorList), line.substring(0, 1))
-                buildLine(builder, SpanStyle(state.colorText), line.substring(1, line.length), !isLastLine(index, lines.size))
-            }
-            line.startsWith(">") -> {
-                buildLine(builder, SpanStyle(state.colorBlockQuote), line.substring(0, 1))
-                buildLine(builder, SpanStyle(state.colorText), line.substring(1, line.length), !isLastLine(index, lines.size))
-            }
-            else -> {
-                buildLine(builder, SpanStyle(state.colorText), line, !isLastLine(index, lines.size))
-            }
-        }
-    }
-
-    return builder.toAnnotatedString()
-}
-
-/**
- * Builds annotated string with provided style and handles line breaks.
- */
-fun buildLine(builder: AnnotatedString.Builder, style: SpanStyle, text: String, breakLine: Boolean = false) {
-    builder.withStyle(style) {
-        append(text)
-
-        if (breakLine)
-            append("\n")
-    }
-}
-
-/**
- * Checks if the current index is the last.
- */
-fun isLastLine(index: Int, size: Int): Boolean {
-    return index == size - 1
 }
