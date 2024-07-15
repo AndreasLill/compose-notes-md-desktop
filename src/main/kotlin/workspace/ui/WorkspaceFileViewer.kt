578a2ab258ad4125b258427f8ba7b8a2d2d583f6
Andreas Lill <dev@andreaslill.com>
2024-07-15 22:03:42 +0200

    Added scrollbar and lazy list for workspace file viewer.

--- src/main/kotlin/workspace/ui/WorkspaceFileViewer.kt
+++ src/main/kotlin/workspace/ui/WorkspaceFileViewer.kt
@@ -2,14 +2,22 @@ package workspace.ui
 
 import androidx.compose.foundation.ContextMenuArea
 import androidx.compose.foundation.ContextMenuItem
+import androidx.compose.foundation.VerticalScrollbar
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import application.model.Action
 import application.model.ApplicationState
 import kotlinx.coroutines.delay
@@ -20,6 +28,7 @@ import workspace.model.WorkspaceViewModel
 fun WorkspaceFileViewer(appState: ApplicationState)  {
     val viewModel = remember(appState.workspace) { WorkspaceViewModel(appState) }
     val scope = rememberCoroutineScope()
+    val lazyListState = rememberLazyListState()
 
     LaunchedEffect(appState.workspace) {
         if (appState.workspace == null) {
@@ -49,138 +58,143 @@ fun WorkspaceFileViewer(appState: ApplicationState)  {
     }
 
     if (appState.workspace != null) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            viewModel.directory.forEach { path ->
-                WorkspaceFile(
-                    path = path,
-                    depth = (path.parent.toString().toCharArray().count { it == '\\' || it == '/' } - appState.workspace.toString().toCharArray().count { it == '\\' || it == '/' }),
-                    visible = path.parent.toString() == appState.workspace.toString() || viewModel.openFolders.contains(path.parent),
-                    unsavedChanges = appState.file == path && appState.unsavedChanges,
-                    isOpenFile = appState.file == path,
-                    isOpenFolder = viewModel.openFolders.contains(path),
-                    onClick = {
-                        if (appState.unsavedChanges) {
-                            appState.confirmDialog.showDialog(
-                                title = "Unsaved Changes",
-                                body = "There are unsaved changes in '${appState.file?.fileName}'\nDo you want to save the changes?",
-                                buttonCancel = "Cancel",
-                                buttonDiscard = "Discard",
-                                buttonConfirm = "Save Changes",
-                                onDiscard = {
-                                    scope.launch {
-                                        appState.discardChanges()
-                                        viewModel.selectItem(path)
+        Box(modifier = Modifier.fillMaxSize()) {
+            LazyColumn(modifier = Modifier.fillMaxSize().padding(end = 8.dp), state = lazyListState) {
+                items(items = viewModel.directory, key = { it }) { path ->
+                    WorkspaceFile(
+                        path = path,
+                        depth = (path.parent.toString().toCharArray().count { it == '\\' || it == '/' } - appState.workspace.toString().toCharArray().count { it == '\\' || it == '/' }),
+                        visible = path.parent.toString() == appState.workspace.toString() || viewModel.openFolders.contains(path.parent),
+                        unsavedChanges = appState.file == path && appState.unsavedChanges,
+                        isOpenFile = appState.file == path,
+                        isOpenFolder = viewModel.openFolders.contains(path),
+                        onClick = {
+                            if (appState.unsavedChanges) {
+                                appState.confirmDialog.showDialog(
+                                    title = "Unsaved Changes",
+                                    body = "There are unsaved changes in '${appState.file?.fileName}'\nDo you want to save the changes?",
+                                    buttonCancel = "Cancel",
+                                    buttonDiscard = "Discard",
+                                    buttonConfirm = "Save Changes",
+                                    onDiscard = {
+                                        scope.launch {
+                                            appState.discardChanges()
+                                            viewModel.selectItem(path)
+                                        }
+                                    },
+                                    onConfirm = {
+                                        scope.launch {
+                                            appState.saveChanges()
+                                            viewModel.selectItem(path)
+                                        }
                                     }
-                                },
-                                onConfirm = {
-                                    scope.launch {
-                                        appState.saveChanges()
-                                        viewModel.selectItem(path)
-                                    }
-                                }
-                            )
-                        } else {
-                            viewModel.selectItem(path)
-                        }
-                    },
-                    onNewFile = {
-                        scope.launch {
-                            viewModel.createFile(path)
-                        }
-                    },
-                    onNewFolder = {
-                        scope.launch {
-                            viewModel.createFolder(path)
-                        }
-                    },
-                    onOpenInExplorer = {
-                        viewModel.openInExplorer(path)
-                    },
-                    onBeginRename = {
-                        if (appState.unsavedChanges) {
-                            appState.confirmDialog.showDialog(
-                                title = "Unsaved Changes",
-                                body = "There are unsaved changes in '${appState.file?.fileName}'\nDo you want to save the changes?",
-                                buttonCancel = "Cancel",
-                                buttonDiscard = "Discard",
-                                buttonConfirm = "Save Changes",
-                                onDiscard = {
-                                    scope.launch {
-                                        appState.discardChanges()
-                                        appState.file = path
-                                    }
-                                },
-                                onConfirm = {
-                                    scope.launch {
-                                        appState.saveChanges()
-                                        appState.file = path
-                                    }
-                                }
-                            )
-                        } else {
-                            appState.file = path
-                        }
-                    },
-                    onRename = {
-                        scope.launch {
-                            viewModel.renamePath(path, it)
-                        }
-                    },
-                    onDelete = {
-                        appState.confirmDialog.showDialog(
-                            title = "Delete",
-                            body = "Are you sure you want to delete '${path.fileName}'?\nIt will be moved to the recycle bin.",
-                            buttonCancel = "Cancel",
-                            buttonConfirm = "Delete",
-                            onCancel = {
-                                println("Cancel")
-                            },
-                            onConfirm = {
-                                scope.launch {
-                                    viewModel.deletePath(path)
-                                }
+                                )
+                            } else {
+                                viewModel.selectItem(path)
                             }
-                        )
-                    },
-                )
-            }
-            ContextMenuArea(
-                items = {
-                    listOf(
-                        ContextMenuItem(
-                            label = "New File",
-                            onClick = {
-                                scope.launch {
-                                    appState.workspace?.let {
-                                        viewModel.createFile(it)
+                        },
+                        onNewFile = {
+                            scope.launch {
+                                viewModel.createFile(path)
+                            }
+                        },
+                        onNewFolder = {
+                            scope.launch {
+                                viewModel.createFolder(path)
+                            }
+                        },
+                        onOpenInExplorer = {
+                            viewModel.openInExplorer(path)
+                        },
+                        onBeginRename = {
+                            if (appState.unsavedChanges) {
+                                appState.confirmDialog.showDialog(
+                                    title = "Unsaved Changes",
+                                    body = "There are unsaved changes in '${appState.file?.fileName}'\nDo you want to save the changes?",
+                                    buttonCancel = "Cancel",
+                                    buttonDiscard = "Discard",
+                                    buttonConfirm = "Save Changes",
+                                    onDiscard = {
+                                        scope.launch {
+                                            appState.discardChanges()
+                                            appState.file = path
+                                        }
+                                    },
+                                    onConfirm = {
+                                        scope.launch {
+                                            appState.saveChanges()
+                                            appState.file = path
+                                        }
+                                    }
+                                )
+                            } else {
+                                appState.file = path
+                            }
+                        },
+                        onRename = {
+                            scope.launch {
+                                viewModel.renamePath(path, it)
+                            }
+                        },
+                        onDelete = {
+                            appState.confirmDialog.showDialog(
+                                title = "Delete",
+                                body = "Are you sure you want to delete '${path.fileName}'?\nIt will be moved to the recycle bin.",
+                                buttonCancel = "Cancel",
+                                buttonConfirm = "Delete",
+                                onCancel = {
+                                    println("Cancel")
+                                },
+                                onConfirm = {
+                                    scope.launch {
+                                        viewModel.deletePath(path)
                                     }
                                 }
-                            },
-                        ),
-                        ContextMenuItem(
-                            label = "New Folder",
-                            onClick = {
-                                scope.launch {
-                                    appState.workspace?.let {
-                                        viewModel.createFolder(it)
-                                    }
-                                }
-                            },
-                        ),
-                        ContextMenuItem(
-                            label = "Open In Explorer",
-                            onClick = {
-                                appState.workspace?.let {
-                                    viewModel.openInExplorer(it)
-                                }
-                            },
-                        ),
+                            )
+                        },
                     )
-                },
-                content = {
-                    Box(modifier = Modifier.fillMaxSize())
                 }
-            )
+                item {
+                    ContextMenuArea(
+                        items = {
+                            listOf(
+                                ContextMenuItem(
+                                    label = "New File",
+                                    onClick = {
+                                        scope.launch {
+                                            appState.workspace?.let {
+                                                viewModel.createFile(it)
+                                            }
+                                        }
+                                    },
+                                ),
+                                ContextMenuItem(
+                                    label = "New Folder",
+                                    onClick = {
+                                        scope.launch {
+                                            appState.workspace?.let {
+                                                viewModel.createFolder(it)
+                                            }
+                                        }
+                                    },
+                                ),
+                                ContextMenuItem(
+                                    label = "Open In Explorer",
+                                    onClick = {
+                                        appState.workspace?.let {
+                                            viewModel.openInExplorer(it)
+                                        }
+                                    },
+                                ),
+                            )
+                        },
+                        content = {
+                            Box(modifier = Modifier.fillMaxSize())
+                        }
+                    )
+                }
+            }
+            VerticalScrollbar(modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(), adapter = rememberScrollbarAdapter(lazyListState))
         }
     }
 }
